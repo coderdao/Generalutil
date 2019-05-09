cc0e01ca9354c1ee25079a785787e643c7f6f53c
abo_o <abo2013@foxmail.com>
2019-05-09 21:56:01 +0800

    文件操作类补充

--- src/Utils/FileUtil.php
+++ src/Utils/FileUtil.php
@@ -17,11 +17,11 @@ class FileUtil
     public function checkDirFileExist( string $filePath )
     {
         $dirPath = dirname( $filePath );
-        if ( !$dirPath ) { throw new ApiException( 500, '系统异常：暂无目录信息' ); }
+        if ( !$dirPath ) { throw new \Exception( 500, '系统异常：暂无目录信息' ); }
 
         //目录不存在则创建目录
         if ( !is_dir( $dirPath ) && !mkdir( $dirPath, 755,true ) ) {
-            throw new ApiException( 500, '系统异常：目录创建失败' );//mkdir没加true是上一级目录不存在就会创建失败
+            throw new \Exception( 500, '系统异常：目录创建失败' );//mkdir没加true是上一级目录不存在就会创建失败
         }
 
         //生成文件存在 则删除，重新创建
@@ -32,21 +32,49 @@ class FileUtil
         return true;
     }
 
-    /**
-     * 文件重命名
-     * @param string $oldFilePath
-     * @param string $newFilePath
-     * @return bool
-     * @throws InnerException
-     */
+    /** 文件重命名 */
     public function rename( string $oldFilePath, string $newFilePath )
     {
         if ( !$newFilePath )
-            { throw new InnerException( '500', '新文件名不能为空:'.$newFilePath ); }
+            { throw new \Exception( '500', '新文件名不能为空:'.$newFilePath ); }
         if ( !$oldFilePath || !file_exists( $oldFilePath ) )
-            { throw new InnerException( '500', '重命名文件不存在:'.$oldFilePath ); }
+            { throw new \Exception( '500', '重命名文件不存在:'.$oldFilePath ); }
         if ( file_exists( $newFilePath ) ) { return '新文件已存在'; }
 
         return rename( $oldFilePath, $newFilePath );
+    }
+
+    /**
+     * 代码模板 创建 实例
+        $createStuFileConfig = [
+            'stubPath' => __DIR__.'/stubs/SyncDataLogic.stub',
+            'createClassPath' => app_path( 'Logic/Larasearch/'. {createClassName} .'.php' ),
+            'targetArray' => [ 'DummyClass', 'DummyTable', ],
+            'replaceArray' => [ 'Sync'. {createClassName} .'Logic', $this->inputName, ],
+        ];
+     */
+    public function createStubFile( array $createStuFileConfig )
+    {
+        if ( !isset( $createStuFileConfig[ 'stubPath' ] ) || !isset( $createStuFileConfig[ 'createClassPath' ] )
+            || !isset( $createStuFileConfig[ 'targetArray' ] ) || !isset( $createStuFileConfig[ 'replaceArray' ] )
+        ) {
+            throw new \Exception( '代码模板 创建 实例中存在必要参数缺失,请检查', false );
+        }
+
+        $FileUtil = new FileUtil();
+
+        // 模板文件源
+        $fileContent = file_get_contents( $createStuFileConfig[ 'stubPath' ] );
+
+        // 替换模板 内容
+        $fileContent = str_replace(
+            $createStuFileConfig[ 'targetArray' ]
+            , $createStuFileConfig[ 'replaceArray' ]
+            , $fileContent
+        );
+
+        // 生成逻辑类
+        $FileUtil->checkDirFileExist( $createStuFileConfig[ 'createClassPath' ] );
+        return file_put_contents( $createStuFileConfig[ 'createClassPath' ], $fileContent );
     }
 }
