56061afd143f4673fa1884bbe9a11c90175095f6
abo_o <abo2013@foxmail.com>
2019-05-14 15:20:27 +0800

    Dto补充

---
+++ src/Dto/ObjectDto.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * Function: 对象 数组 转换, 需要往下自定义
+ * Author: YUN
+ * Date: 2018/5/31 下午2:14
+ * Description:
+ */
+
+namespace Abo\Fasterapi\Dto\BaseDto;
+
+
+class ObjectDto
+{
+    /**
+     * 转对象
+     * @param $data
+     * @throws \ReflectionException
+     */
+    public function fromArray($data)
+    {
+        $class_name = get_class($this);
+        $class = new \ReflectionClass($class_name);
+        $methods = $class->getMethods();
+
+        foreach ($methods as $value) {
+            if($value->class == $class_name) {
+                $methodAccess = new \ReflectionMethod($class_name,$value->name);
+                if($methodAccess->isPublic()) {
+                    $func_name = $this->uncamelize($value->name);
+                    if (strpos($func_name, 'set_') === 0 && strlen($func_name)> 4 ) {
+                        $set_func = $value->name;
+                        $prop_name = substr($func_name, 4);
+
+                        if (isset($data[$prop_name])) {
+                            $this->$set_func($data[$prop_name]);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * 转数组
+     * @param bool $ignore_null 是否忽略NULL值
+     * @return array
+     */
+    public function toArray($ignore_null=true)
+    {
+        $class_name = get_class($this);
+        $class = new \ReflectionClass($class_name);
+        $methods = $class->getMethods();
+
+        $data =[];
+        foreach ($methods as $value) {
+            if($value->class == $class_name) {
+                $methodAccess = new \ReflectionMethod($class_name,$value->name);
+                if($methodAccess->isPublic()) {
+                    $func_name = uncamelize($value->name);
+                    if (strpos($func_name, 'get_') === 0 && strlen($func_name)> 4 ) {
+                        $get_func = $value->name;
+                        $prop_name = substr($func_name, 4);
+                        $v = $this->$get_func();
+
+                        if (is_null($v)) {
+                            if ( $ignore_null == false) {
+                                $data[$prop_name] = null;
+                            }
+                        } else {
+                            $data[$prop_name] = $v;
+                        }
+                    }
+                }
+            }
+        }
+
+        return $data;
+    }
+}
---
+++ src/Dto/RequestDto.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Function: 为了躲开 undefined index
+ * Description: 当开启 error_reporting = E_ALL~E_NOTICE时,躲开保存,同时得到 数据传输类
+ * Abo 2019/1/18 15:22
+ * Email: abo2013@foxmail.com
+ */
+
+namespace Abo\Fasterapi\Dto\BaseDto;
+
+use Illuminate\Http\Request;
+
+class RequestDto
+{
+    const DEFAULT_VALUE = '';
+    protected $urlParams, $request;
+
+    public function __construct()
+    {
+        $request = Request();
+        $this->request = $request;
+        $this->urlParams = $request->all();
+    }
+
+    /** 获取请求参数 */
+    public function request( string $key = '', $default = false, string $filter = '' )
+    {
+        if ( !$key ) { return $this->urlParams; }
+        $ret2Default = $default ?:self::DEFAULT_VALUE;
+
+        if ( !isset( $this->urlParams[$key] ) ) {
+            return $ret2Default;
+        }
+
+        $ret2Default = $this->urlParams[$key];
+        if ( $filter && function_exists( $filter ) ) {
+            $ret2Default = $filter( $this->urlParams[$key] );
+        }
+        return $ret2Default; // array_key_exists( $key, $this->urlParams )
+    }
+
+
+    /** 是否存在请求参数 */
+    public function hasKey( string $key = '' )
+    {
+        return isset( $key, $this->urlParams );
+    }
+
+    /** 获取 Request 请求对象 @return Request */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    public function setUrlParam( string $key, $value = '' )
+    {
+        $this->urlParams[ $key ] = $value;
+    }
+
+    /** 获取当前页数 */
+    public function getPageNow( int $pageNow = 1 )
+    {
+        if ( $this->hasKey( 'pageNow' ) ) {
+            $pageNow = $this->request('pageNow', $pageNow, 'intval');
+        }
+        if ( $this->hasKey( 'page' ) ) {
+            $pageNow = $this->request( 'page', $pageNow, 'intval');
+        }
+        if ( $this->hasKey( 'p' ) ) {
+            $pageNow = $this->request( 'p', $pageNow, 'intval');
+        }
+
+        return ( $pageNow >= 1 ?:1 );
+    }
+
+    /** 获取当前每页数量 */
+    public function getPageSize( int $pageSize = 20 )
+    {
+        if ( $this->hasKey( 'pageSize' ) ) {
+            $pageSize = $this->request('pageSize', $pageSize, 'intval');
+        }
+        if ( $this->hasKey( 'page' ) ) {
+            $pageSize = $this->request( 'pagesize', $pageSize, 'intval');
+        }
+
+        return ( $pageSize >= 20 ?:20 );
+    }
+}
