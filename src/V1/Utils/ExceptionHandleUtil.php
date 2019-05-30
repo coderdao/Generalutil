986b5033ac90857d5aeb6213baa44d285b263b35
abo_o <abo2013@foxmail.com>
2019-05-30 22:13:43 +0800

    补充

--- src/V1/Utils/ExceptionHandleUtil.php
+++ src/V1/Utils/ExceptionHandleUtil.php
@@ -9,16 +9,23 @@
 namespace Abo\Generalutil\V1\Utils;
 
 
+use Abo\Generalutil\V1\Exceptions\ApiException;
+use Abo\Generalutil\V1\Exceptions\PageException;
+
 class ExceptionHandleUtil
 {
     public static function handle( \Exception $exception )
     {
+        $code = $exception->getCode();
+
         /* 错误页面 */
-        if ($exception instanceof PageException) {
-            $code = $exception->getCode();
-            if (view()->exists('errors.errorMsg')) {
-                return response()->view('errors.errorMsg', ['msg'=>$exception->getMessage()], $code);
-            }
+        if ( $exception instanceof PageException ) { // todo view 加载模板路径处理
+            return response()->view('errors.errorMsg', ['msg'=>$exception->getMessage()], $code);
+            exit();
+        }
+
+        if ( $exception instanceof ApiException ) {
+            return ResponseUtil::error()->message( $exception->getMessage() )->json();
         }
     }
 }
