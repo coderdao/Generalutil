35d79edcaff1e395699b5d954eff66495be8292f
abo_o <abo2013@foxmail.com>
2019-06-03 15:06:13 +0800

    更新config util

--- src/V1/Utils/ConfigUtil.php
+++ src/V1/Utils/ConfigUtil.php
@@ -6,176 +6,32 @@ use ArrayAccess;
 use Illuminate\Support\Arr;
 use Illuminate\Contracts\Config\Repository as ConfigContract;
 
-class ConfigUtil implements ArrayAccess, ConfigContract
+class ConfigUtil
 {
-    /**
-     * All of the configuration items.
-     *
-     * @var array
-     */
-    protected $items = [];
+    public $configPath;
+    public $items = [];
 
-    /**
-     * Create a new configuration repository.
-     *
-     * @param  array  $items
-     * @return void
-     */
-    public function __construct( string $configName )
+    public function __construct( string $configPath )
     {
-        $items = $this->getConfigFromFile( $configName );
-        $this->items = $items;
+        $this->configPath = $configPath;
+        $this->items = $this->getConfigFromFile( $configPath );
     }
 
-    /**
-     * Determine if the given configuration value exists.
-     *
-     * @param  string  $key
-     * @return bool
-     */
-    public function has($key)
+    public function get( $key, $default = null )
     {
-        return Arr::has($this->items, $key);
-    }
+        $temItems = $this->items;
+        if ( strstr( $key, '.' ) ) {
+            $keys = explode( '.', $key );
 
-    /**
-     * Get the specified configuration value.
-     *
-     * @param  array|string  $key
-     * @param  mixed   $default
-     * @return mixed
-     */
-    public function get($key, $default = null)
-    {
-        if (is_array($key)) {
-            return $this->getMany($key);
-        }
-
-        return Arr::get($this->items, $key, $default);
-    }
-
-    /**
-     * Get many configuration values.
-     *
-     * @param  array  $keys
-     * @return array
-     */
-    public function getMany($keys)
-    {
-        $config = [];
-
-        foreach ($keys as $key => $default) {
-            if (is_numeric($key)) {
-                list($key, $default) = [$default, null];
+            foreach ( $keys as $v2Key ) {
+                $temItems = isset( $temItems[ $v2Key ] ) ? $temItems[ $v2Key ] : $default ;
             }
-
-            $config[$key] = Arr::get($this->items, $key, $default);
+            $ret2Return = $temItems;
+        } else {
+            $ret2Return = isset( $temItems[ $key ] ) ? $temItems[ $key ] : $default ;
         }
 
-        return $config;
-    }
-
-    /**
-     * Set a given configuration value.
-     *
-     * @param  array|string  $key
-     * @param  mixed   $value
-     * @return void
-     */
-    public function set($key, $value = null)
-    {
-        $keys = is_array($key) ? $key : [$key => $value];
-
-        foreach ($keys as $key => $value) {
-            Arr::set($this->items, $key, $value);
-        }
-    }
-
-    /**
-     * Prepend a value onto an array configuration value.
-     *
-     * @param  string  $key
-     * @param  mixed  $value
-     * @return void
-     */
-    public function prepend($key, $value)
-    {
-        $array = $this->get($key);
-
-        array_unshift($array, $value);
-
-        $this->set($key, $array);
-    }
-
-    /**
-     * Push a value onto an array configuration value.
-     *
-     * @param  string  $key
-     * @param  mixed  $value
-     * @return void
-     */
-    public function push($key, $value)
-    {
-        $array = $this->get($key);
-
-        $array[] = $value;
-
-        $this->set($key, $array);
-    }
-
-    /**
-     * Get all of the configuration items for the application.
-     *
-     * @return array
-     */
-    public function all()
-    {
-        return $this->items;
-    }
-
-    /**
-     * Determine if the given configuration option exists.
-     *
-     * @param  string  $key
-     * @return bool
-     */
-    public function offsetExists($key)
-    {
-        return $this->has($key);
-    }
-
-    /**
-     * Get a configuration option.
-     *
-     * @param  string  $key
-     * @return mixed
-     */
-    public function offsetGet($key)
-    {
-        return $this->get($key);
-    }
-
-    /**
-     * Set a configuration option.
-     *
-     * @param  string  $key
-     * @param  mixed  $value
-     * @return void
-     */
-    public function offsetSet($key, $value)
-    {
-        $this->set($key, $value);
-    }
-
-    /**
-     * Unset a configuration option.
-     *
-     * @param  string  $key
-     * @return void
-     */
-    public function offsetUnset($key)
-    {
-        $this->set($key, null);
+        return $ret2Return;
     }
 
     /**
@@ -186,7 +42,11 @@ class ConfigUtil implements ArrayAccess, ConfigContract
     protected function getConfigFromFile( $configName )
     {
         $configPath = __DIR__.'/../Config/'.$configName;
-        if ( !$configName || !file_exists( $configPath ) ) {
+        if ( file_exists( $configName ) ) {
+            $configPath = $configName;
+        }
+
+        if ( !file_exists( $configPath ) ) {
             return [];
         }
 
